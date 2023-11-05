e955ec669e0816dc2de66728662a750b91379269
Rstar-1 <rajshetye.5855@gmail.com>
2023-11-05 10:51:14 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./index.css";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Error from "./pages/error/Error";
-import Navbar from "./pages/navbar/Navbar";
-import Footer from "./pages/footer/Footer";
+import Error from "./websites/error/Error";
+import Navbar from "./websites/navbar/Navbar";
+import Footer from "./websites/footer/Footer";
 
-const  App = () => {
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  };
+const App = () => {
   return (
     <div className="App relative">
       <Router>
@@ -25,20 +21,8 @@ const  App = () => {
         </Routes>
         <Footer />
       </Router>
-      <div className="fixed bottom-0 right-0">
-        <div
-          className="bgsecondary cursor-pointer up-box rounded-full flex justify-center items-center m10"
-          onClick={scrollToTop}
-        >
-          <img
-            src="https://i.imgur.com/MEcgvFc.png"
-            alt="upimg"
-            className="up-img"
-          />
-        </div>
-      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div className="error-h w-full relative">
+    <div className="error-h w-full relative bgwhite border-d mtpx9">
       <img
         src="https://imgur.com/FkHCrdg.jpg"
         alt="error"
-        className="error-h w-full"
+        className="error-h border-d w-full"
       />
       <div className="absolute top-0 left-0 w-full">
         <div className="container mx-auto">
@@ -19,7 +19,7 @@ const Error = () => {
               </h6>
               <div className="flex justify-center">
                 <NavLink to="/home">
-                  <button className="bgp-outline fsize16 sm-fsize13 font-500 cursor-pointer plpx30 prpx30 sm-mtpx5 sm-ptpx10 sm-pbpx10 ptpx12 pbpx12 rounded-5">
+                  <button className="whitebtn fsize16 sm-fsize13 font-500 cursor-pointer plpx30 prpx30 sm-mtpx5 sm-ptpx10 sm-pbpx10 ptpx12 pbpx12 rounded-5">
                     back to Home
                   </button>
                 </NavLink>
